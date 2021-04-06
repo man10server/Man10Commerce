a045e4ffd9689985fd27f1107f1623886d8dcf04
forest611 <forest611328@gmail.com>
2021-04-06 10:44:48 +0900

    Initial commit

---
+++ src/main/java/red/man10/man10commerce/Man10Commerce.java
@@ -0,0 +1,17 @@
+package red.man10.man10commerce;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Man10Commerce extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
