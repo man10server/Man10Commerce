98d53a618016891b971fef2b03c1777a171fc464
forest611 <forest611328@gmail.com>
2021-09-12 17:12:35 +0900

    ログの処理を修正

--- src/main/java/red/man10/man10commerce/data/Log.kt
+++ src/main/java/red/man10/man10commerce/data/Log.kt
@@ -3,19 +3,17 @@ package red.man10.man10commerce.data
 import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
-import red.man10.man10commerce.Man10Commerce
+import red.man10.man10commerce.data.MySQLManager.Companion.mysqlQueue
 
 object Log {
-
-    private val mysql = MySQLManager(Man10Commerce.plugin,"Man10CommerceLog")
 
     fun sellLog(p:Player,item: ItemStack,price:Double,itemID:Int){
 
         val name = if (item.hasItemMeta()) item.itemMeta!!.displayName else item.i18NDisplayName
-        mysql.execute("INSERT INTO log " +
+
+        mysqlQueue.add("INSERT INTO log " +
                 "(order_player, target_player, action, item_id, item_name, amount, price, date) " +
                 "VALUES ('${p.name}', '','SellItem' , $itemID, '${name}', ${item.amount}, ${price}, now())")
-
     }
 
     fun buyLog(p:Player,data: Data,item:ItemStack){
@@ -23,7 +21,7 @@ object Log {
         val order = Bukkit.getOfflinePlayer(data.seller!!)
 
         val name = if (item.hasItemMeta()) item.itemMeta!!.displayName else item.i18NDisplayName
-        mysql.execute("INSERT INTO log " +
+        mysqlQueue.add("INSERT INTO log " +
                 "(order_player, target_player, action, item_id, item_name, amount, price, date) " +
                 "VALUES ('${order.name}', '${p.name}','BuyItem' , ${data.itemID}, '${name}', ${item.amount}, ${data.price}, now())")
     }
@@ -31,10 +29,10 @@ object Log {
     fun closeLog(p:Player,itemID:Int,item: ItemStack){
 
         val name = if (item.hasItemMeta()) item.itemMeta!!.displayName else item.i18NDisplayName
-        mysql.execute("INSERT INTO log " +
+
+        mysqlQueue.add("INSERT INTO log " +
                 "(order_player, target_player, action, item_id, item_name, amount, price, date) " +
                 "VALUES ('${p.name}', '','CloseItem' , ${itemID}, '${name}', ${item.amount}, 0, now())")
-
 
     }
 
